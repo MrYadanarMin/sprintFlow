d5ac4dfae8a31c92424ff4e06daa28d4b2888f81
MrYadanarMin <treasureking.cz@gmail.com>
2026-09-19 21:01:05 +0800

    Add sprintflow boostrap blade templates

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('board');
+});
+
+Route::get('/board', function () {
+    return view('board');
+});
+
+Route::get('/backlog', function () {
+    return view('backlog');
+});
+
+Route::get('/issue-detail', function () {
+    return view('issue-detail');
 });
